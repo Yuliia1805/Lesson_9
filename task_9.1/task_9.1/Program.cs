bec2d0273a570251e8c18da9dac3321b35c6f44b
yuliia <student@D03>
2017-09-02 11:30:36 +0300

    init commit

---
+++ task_9.1/task_9.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 1, 2, 3, 4, 5, 6, 10, 35, 457, 5, 79 };
+            foreach ( int i in mas)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task_9.2/task_9.2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int minVal = -100;
+            const int maxVal = 100;
+            int min;
+            int max;
+            Console.Write("Enter size of  array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Random rand = new Random();
+
+            for ( int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(minVal, maxVal);
+            }
+            
+           
+           /* min = array[0];
+            max = array[0];
+            for ( int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Minimum: " + min);
+            Console.WriteLine("Maximum: " + max);
+
+            int[] newArray = new int[size];
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = rand.Next(min, max);
+            }*/
+
+           /* for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - 1; j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            int temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+
+                }
+                Console.WriteLine("\nSorted array: ");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }*/
+
+            Console.WriteLine("Original array: ");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            int[] reverseArray = new int[size];
+            ///Array.Reverse(array);
+            for( int i = 0; i < reverseArray.Length; i++)
+            {
+                reverseArray[i] = array[array.Length - i - 1];
+            }
+
+
+            Console.WriteLine("\nReversed array: ");
+            for (int i = 0; i < reverseArray.Length; i++)
+            {
+                Console.Write(reverseArray[i] + " ");
+            }
+
+            /*for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+
+            }
+            Console.WriteLine("\nSorted array: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }*/
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
